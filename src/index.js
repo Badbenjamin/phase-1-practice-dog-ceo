7a63850b60eaa01b927d4c5075b05a7f976f9799
ben mccarthy <bmccarthy90@gmail.com>
2024-07-22 22:01:04 -0400

    dog images come up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+
+const addDogPics = () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then(data => {
+        const dogImageContainer = document.getElementById('dog-image-container')
+        data.message.forEach((element) => {
+            dogImages = document.createElement('img')
+            dogImages.src = element;
+            dogImageContainer.appendChild(dogImages)
+        })
+    })
+}
+    
+const addDogBreeds = () => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    console.log(response)
+}
+
+const init = () => {
+    document.addEventListener("DOMContentLoaded", () => {
+        addDogPics()
+        addDogBreeds()
+     });
+}  
+
+init()
+
+
+
