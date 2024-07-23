aebdbccaa3d9a67ce681cc788fe73fa03298008f
ben mccarthy <bmccarthy90@gmail.com>
2024-07-23 13:53:53 -0400

    trying to get breed list to iterate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+// having trouble accessing info from dropdown
+function getDropDownInput() {
+    // console.log("hello")
+    const dropDownMenu = document.getElementById('breed-dropdown')
+    const dropDownValue = dropDownMenu.getElementsByTagName('option');
+    // console.log(dropDownValue[0])
+    dropDownMenu.addEventListener('select', event => {
+        event.preventDefault();
+    })
+}
+
+function displayDogDetails (element) {
+    // console.log(element)
+}
 
 const addDogPics = () => {
     fetch("https://dog.ceo/api/breeds/image/random/4")
@@ -9,20 +24,29 @@ const addDogPics = () => {
             dogImages = document.createElement('img')
             dogImages.src = element;
             dogImageContainer.appendChild(dogImages)
+
+            dogImages.addEventListener('click', () => {
+                displayDogDetails(element);
+            })
         })
     })
 }
     
 const addDogBreeds = () => {
     fetch("https://dog.ceo/api/breeds/list/all")
-    .then((response) => response.json())
-    console.log(response)
-}
+    .then(response => response.json())
+    .then((dogs) => {
+        console.log(dogs)
+    })
+ }
+
+    
 
 const init = () => {
     document.addEventListener("DOMContentLoaded", () => {
         addDogPics()
         addDogBreeds()
+        getDropDownInput()
      });
 }  
 
